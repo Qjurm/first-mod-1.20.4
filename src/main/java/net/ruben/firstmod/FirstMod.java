6faba07e1cb36ad685ef493289a88108b0414eee
Qjurm <ruben.leus2005@gmail.com>
2024-03-12 16:13:40 +0100

    first commit

---
+++ src/main/java/net/ruben/firstmod/FirstMod.java
@@ -0,0 +1,19 @@
+package net.ruben.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstMod implements ModInitializer {
+	public static final String MOD_ID = "first-mod";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+
+	}
+}
---
+++ src/main/java/net/ruben/firstmod/FistModClient.java
@@ -0,0 +1,10 @@
+package net.ruben.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FistModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
